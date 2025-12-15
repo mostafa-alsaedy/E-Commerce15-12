c798a77f5b2eaf44c9fba2da01b44868bd252a53
mostafa-alsaedy <mostafa.saedy99@gmail.com>
2025-12-15 14:12:05 +0200

    token update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,10 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-    const decodedToken = (await cookies()).get("next-auth.session-token")?.value
+    const myCookies = await cookies()
+    const decodedToken = myCookies.get("next-auth.session-token")?.value || myCookies.get("__secure-next-auth.session-token")?.value 
     const token = await decode({ token: decodedToken, secret: process.env.AUTH_SECRET! })
     console.log(token?.token);
-    
+
     return token?.token as string
 }
